8a42f254a143e2ce13023d30dc9c0a84e2162db3
yasaichi <yasaichi@users.noreply.github.com>
2018-04-05 00:44:44 +0900

    5.3.1.2

--- twittervotes/twitter.go
+++ twittervotes/twitter.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"context"
+	"github.com/garyburd/go-oauth/oauth"
+	"github.com/joeshaw/envdecode"
 	"io"
+	"log"
 	"net"
+	"net/http"
+	"net/url"
+	"strconv"
+	"sync"
 	"time"
 )
 
 var conn net.Conn
 
-func dial(netw, addr string) (net.Conn, error) {
+func dialContext(_ context.Context, netw, addr string) (net.Conn, error) {
 	if conn != nil {
 		conn.Close()
 		conn = nil
@@ -33,4 +41,59 @@ func closeConn() {
 	if reader != nil {
 		reader.Close()
 	}
+}
+
+var (
+	authClient *oauth.Client
+	creds      *oauth.Credentials
+)
+
+func setupTwitterAuth() {
+	var ts struct {
+		ConsumerKey    string `env:"SP_TWITTER_KEY,required"`
+		ConsumerSecret string `env:"SP_TWITTER_SECRET,required"`
+		AccessToken    string `env:"SP_TWITTER_ACCESSTOKEN,required"`
+		AccessSecret   string `env:"SP_TWITTER_ACCESSSECRET,required"`
+	}
+	if err := envdecode.Decode(&ts); err != nil {
+		log.Fatalln(err)
+	}
+
+	authClient = &oauth.Client{
+		Credentials: oauth.Credentials{
+			Token:  ts.ConsumerKey,
+			Secret: ts.ConsumerSecret,
+		},
+	}
+	creds = &oauth.Credentials{
+		Token:  ts.AccessToken,
+		Secret: ts.AccessSecret,
+	}
+}
+
+var (
+	authSetupOnce sync.Once
+	httpClient    *http.Client
+)
+
+func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
+	authSetupOnce.Do(func() {
+		setupTwitterAuth()
+		httpClient = &http.Client{
+			Transport: &http.Transport{
+				DialContext: dialContext,
+			},
+		}
+	})
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Length", strconv.Itoa(len(params.Encode()))) // Itoa stands for "Integer to ASCII"
+
+	// See http://tools.ietf.org/html/rfc5849#section-3.5.1
+	err := authClient.SetAuthorizationHeader(req.Header, creds, "POST", req.URL, params)
+	if err != nil {
+		return nil, err
+	}
+
+	return httpClient.Do(req)
 }
