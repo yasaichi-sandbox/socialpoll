1edae594196a25264f37d5ec5fad04c27e8e9c6b
yasaichi <yasaichi@users.noreply.github.com>
2018-04-06 00:52:00 +0900

    5.3.2

--- twittervotes/main.go
+++ twittervotes/main.go
@@ -1,3 +1,47 @@
 package main
 
+import (
+	"gopkg.in/mgo.v2"
+	"log"
+)
+
+var db *mgo.Session
+
+func dialdb() error {
+	var err error
+
+	log.Println("MongoDBにダイヤル中: localhost")
+	db, err = mgo.Dial("localhost")
+
+	return err
+}
+
+func closedb() {
+	if db == nil {
+		return
+	}
+
+	db.Close()
+	log.Println("データベース接続が閉じられました")
+}
+
+type poll struct {
+	Options []string
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+
+	// nil stands for "no filtering"
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+
+	var p poll
+	for iter.Next(&p) {
+		options = append(options, p.Options...)
+	}
+	iter.Close()
+
+	return options, iter.Err()
+}
+
 func main() {}
