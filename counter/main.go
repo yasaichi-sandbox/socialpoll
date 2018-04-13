c77ca0c8094720bf012fa4bbf9146b9d3b6a2309
yasaichi <yasaichi@users.noreply.github.com>
2018-04-14 00:18:24 +0900

    5.4.2

--- counter/main.go
+++ counter/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/bitly/go-nsq"
 	"gopkg.in/mgo.v2"
 	"log"
 	"os"
+	"sync"
 )
 
 var fatalErr error
@@ -36,4 +38,34 @@ func main() {
 
 	pollData := db.DB("ballots").C("polls")
 	_ = pollData
+
+	var countsLock sync.Mutex
+	var counts map[string]int
+
+	log.Println("NSQに接続します...")
+	consumer, err := nsq.NewConsumer("votes", "counter", nsq.NewConfig())
+	if err != nil {
+		fatal(err)
+		return
+	}
+
+	// NOTE: `nsq.HandlerFunc()` does "Type conversion"
+	consumer.AddHandler(nsq.HandlerFunc(func(m *nsq.Message) error {
+		countsLock.Lock()
+		defer countsLock.Unlock()
+
+		if counts == nil {
+			counts = make(map[string]int)
+		}
+
+		vote := string(m.Body)
+		counts[vote]++
+
+		return nil
+	}))
+
+	if err := consumer.ConnectToNSQLookupd("localhost:4161"); err != nil {
+		fatal(err)
+		return
+	}
 }
