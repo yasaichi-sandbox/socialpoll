f6dacc6017b4dcd1ed69a2cacf45b87dec995158
yasaichi <yasaichi@users.noreply.github.com>
2018-04-24 00:03:15 +0900

    6.3.1

--- api/main.go
+++ api/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"net/http"
+)
 
+func main() {}
+
+func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !isValidAPIKey(r.URL.Query().Get("key")) {
+			respondErr(w, r, http.StatusUnauthorized, "不正なAPIキーです")
+			return
+		}
+
+		fn(w, r)
+	}
+}
+
+func isValidAPIKey(key string) bool {
+	return key == "abc123"
+}
+
+func respondErr(w http.ResponseWriter, r *http.Request, status int, args ...interface{}) {
+	// TODO: Implement later
 }
