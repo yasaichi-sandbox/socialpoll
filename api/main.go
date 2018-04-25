b2a94b6f4ba31b23244a0f0c8b264a6feafee5d2
yasaichi <yasaichi@users.noreply.github.com>
2018-04-25 10:12:28 +0900

    6.3.4

--- api/main.go
+++ api/main.go
@@ -44,3 +44,12 @@ func withVars(fn http.HandlerFunc) http.HandlerFunc {
 		fn(w, r)
 	}
 }
+
+func withCORS(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Expose-Headers", "Location")
+
+		fn(w, r)
+	}
+}
