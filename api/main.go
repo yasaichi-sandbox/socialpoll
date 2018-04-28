3f4cb2a877c78fe99fca792bfa6c0ac023e49c67
yasaichi <yasaichi@users.noreply.github.com>
2018-04-28 13:09:44 +0900

    6.6

--- api/main.go
+++ api/main.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"flag"
+	"github.com/stretchr/graceful"
 	"gopkg.in/mgo.v2"
+	"log"
 	"net/http"
+	"time"
 )
 
-func main() {}
+func main() {
+	addr := flag.String("addr", ":8080", "エンドポイントのアドレス")
+	mongo := flag.String("mongo", "localhost", "MongoDBのアドレス")
+	flag.Parse()
+
+	log.Println("MongoDBに接続します", *mongo)
+	db, err := mgo.Dial(*mongo)
+	if err != nil {
+		log.Fatalln("MongoDBへの接続に失敗しました:", err)
+	}
+	defer db.Close()
+
+	// See also: https://stackoverflow.com/questions/36921190/difference-between-http-and-default-servemux
+	mux := http.NewServeMux() // `Mux` stands for multiplexer
+	mux.HandleFunc("/polls/", withCORS(withVars(withData(db, withAPIKey(handlePolls)))))
+
+	log.Println("Webサーバーを開始します:", *addr)
+	graceful.Run(*addr, 1*time.Second, mux)
+	log.Println("停止します...")
+}
+
+func handlePolls(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement the body later
+}
 
 func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
