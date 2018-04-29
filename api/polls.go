b1695f7a4f7b584e9b0a7d50fdbcdf2235c2eb1a
yasaichi <yasaichi@users.noreply.github.com>
2018-04-29 13:06:17 +0900

    6.7.2.1

--- api/polls.go
+++ api/polls.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 	"net/http"
 )
@@ -30,7 +31,23 @@ func handlePolls(w http.ResponseWriter, r *http.Request) {
 }
 
 func handlePollsGet(w http.ResponseWriter, r *http.Request) {
-	respondErr(w, r, http.StatusInternalServerError, errors.New("未実装です"))
+	db := GetVar(r, "db").(*mgo.Database)
+	c := db.C("polls")
+	var q *mgo.Query
+
+	if p := NewPath(r.URL.Path); p.HasID() {
+		q = c.FindId(bson.ObjectIdHex(p.ID))
+	} else {
+		q = c.Find(nil)
+	}
+
+	var result []*poll
+	if err := q.All(&result); err != nil {
+		respondErr(w, r, http.StatusInternalServerError, err)
+		return
+	}
+
+	respond(w, r, http.StatusOK, &result)
 }
 
 func handlePollsPost(w http.ResponseWriter, r *http.Request) {
